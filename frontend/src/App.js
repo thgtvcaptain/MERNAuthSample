a69e21156481906a6d2328b1a22a392e58d6dc23
thgtvcaptain <88418059+thgtvhe7031@users.noreply.github.com>
2023-01-16 17:10:19 +0700

    add Signup and Login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,8 +14,16 @@ function App() {
         <div className="pages">
           <Routes>
             <Route 
-              path="/"
+              exact path="/"
               element={<Home />}
+            />
+            <Route 
+              path="/login"
+              element={<Login/>}
+            />
+            <Route 
+              path="/signup"
+              element={<Signup/>}
             />
           </Routes>
         </div>
